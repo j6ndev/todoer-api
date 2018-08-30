b19ff73c5f78fd79eb39c395d8eac52bd8c6c44e
Jonathan Vazquez <vamtha24@gmail.com>
2018-08-29 21:03:29 -0500

    Add Task router to the app

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { task } = require('@todoer/api');
 
 const app = express();
 const PORT = 8888;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/status', (req, res) => {
   const localTime = new Date().toLocaleTimeString();
   res.status(200).send(`Server time is ${localTime}.`);
 });
+
+app.use('/task', task);
 
 app.get('*', (req, res) => {
   res.sendStatus(404);
