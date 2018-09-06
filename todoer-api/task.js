3485193348ae2c192ec090d24963b29ce1f07559
Jonathan Vazquez <vamtha24@gmail.com>
2018-09-05 20:20:39 -0500

    Add POST /task route

--- todoer-api/task.js
+++ todoer-api/task.js
@@ -1,8 +1,22 @@
 const { Router } = require('express');
+const { Task } = require('@todoer/db');
 
 const router =  new Router();
 
-router.get('/', TODO);
+router.get('/', (req, res) => {
+  Task.find()
+    .then(tasks => res.send(tasks))
+    .catch(e => res.status(400).send(e));
+});
+
+router.post('/', (req, res) => {
+  const task = new Task({ text: req.body.text });
+  task.save()
+    .then(task => res.send(task))
+    .catch(e => res.status(400).send(e));
+});
+
+
 router.get('/:taskId', TODO);
 router.put('/:taskId', TODO);
 router.delete('/:taskId', TODO);
