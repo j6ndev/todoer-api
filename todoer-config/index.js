fe5ccc63b4ba86fd88d751dd4b52adbf9bd2ce78
Jonathan Vazquez <vamtha24@gmail.com>
2018-09-13 17:27:57 -0500

    Refactor config file

--- todoer-config/index.js
+++ todoer-config/index.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 
+const Joi = require('joi');
+
+const configSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .allow(['dev', 'prod', 'test'])
+    .default('dev'),
+  PORT: Joi.number()
+    .default('8888'),
+  MONGODB_URL: Joi.string().required()
+    .description('MongoDB Url')
+}).unknown()
+  .required();
+
+const { error, value: env } = Joi.validate(process.env, configSchema);
+
+if(error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
 const config = {
-  port: process.env.PORT || '8888',
-  db: process.env.MONGODB_URL || 'mongodb://localhost/todoer'
+  enviroment: env.NODE_ENV,
+  port: env.PORT,
+  db: env.MONGODB_URL
 };
 
 module.exports = config;
